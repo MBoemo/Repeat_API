2bc78e2484d2cc788453cba5cc739dce13cd3fb3
Michael Boemo <m.a.boemo@gmail.com>
2019-11-19 17:02:36 +0000

    added rev complement and minimum bed length

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,11 @@
 import Repeat_API as rep
 
-reference = 'simple.fasta'
-query = 'GC'
+reference = '/home/michael/data/genomes/SacCer3.fasta'#'simple.fasta'
+query = 'GGGT'
+lookOnRevComplement = True
+minimumRepeatLength = None #should be none or positive int
 
 fasta = rep.IO(reference)
-info = rep.findRepeats(fasta,query)
-rep.writeBed(query+'.bed',info)
+info = rep.findRepeats(fasta,query,lookOnRevComplement)
+rep.writeBed(query+'.bed',info,minimumRepeatLength)
 rep.freqPlot(query+'.pdf',info)
